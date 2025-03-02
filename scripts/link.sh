52bdab638ec6a20a67c85d57ce855dcd7e798861
ta-tabox <tanktabox@gmail.com>
2025-03-02 14:41:26 +0900

    link: link, unlinkスクリプトを整理

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
 
 # 関数の読み込み
-dotfiles_root=$(cd $(dirname $0)/.. && pwd)
-. ${dotfiles_root}/scripts/common.sh
+dotfiles_root=$(cd $(dirname "$0")/.. && pwd)
+. "${dotfiles_root}/scripts/common.sh"
 
 # シンボリックリンクを作成
-cd ${dotfiles_root}/dotfiles
+cd "${dotfiles_root}/dotfiles" || return
 linklist="linklist.txt"
 
-[ ! -r "${linklist}" ] && continue
+# linklist.txt が存在しない場合は終了
+if [ ! -r "${linklist}" ]; then
+  echo "Error: ${linklist} not found."
+  exit 0
+fi
 
-__remove_linklist_comment "$linklist" | while read target link; do
-	# ~ や環境変数を展開
-	target=$(eval echo "${PWD}/${target}")
-	link=$(eval echo "${link}")
-	# シンボリックリンクを作成
-	__mkdir $(dirname ${link})
-	__ln "${target}" "${link}"
+# linklist.txt を読み込み、シンボリックリンクを作成
+__remove_linklist_comment "${linklist}" | while IFS= read -r line; do
+  # 空行やコメント行はスキップ
+  if [ -z "${line}" ] || [ "${line:0:1}" = "#" ]; then
+    continue
+  fi
+
+  # target と link を分割
+  IFS=" " read -r target link <<< "${line}"
+
+  # 変数を展開
+  target="${PWD}/${target}"
+  link="${link/#\~/${HOME}}"
+
+  # シンボリックリンクを作成
+  __mkdir "$(dirname "${link}")"
+  if ! __ln "${target}" "${link}"; then
+    echo "Error: Failed to create symbolic link: ${link}"
+  fi
 done
--- scripts/unlink.sh
+++ scripts/unlink.sh
@@ -1,17 +1,33 @@
 #!/bin/sh
 
-dotfiles_root=$(cd $(dirname $0)/.. && pwd)
-. ${dotfiles_root}/scripts/common.sh
+# 関数の読み込み
+dotfiles_root=$(cd $(dirname "$0")/.. && pwd)
+. "${dotfiles_root}/scripts/common.sh"
 
 # シンボリックリンクを削除
-cd ${dotfiles_root}/dotfiles
+cd "${dotfiles_root}/dotfiles" || return
 linklist="linklist.txt"
 
-[ ! -r "${linklist}" ] && continue
+# linklist.txt が存在しない場合は終了
+if [ ! -r "${linklist}" ]; then
+  echo "Error: ${linklist} not found."
+  exit 0
+fi
 
-__remove_linklist_comment "$linklist" | while read target link; do
-	# ~ や環境変数を展開
-	link=$(eval echo "${link}")
-	# シンボリックリンクを作成
-	__unlink ${link}
+__remove_linklist_comment "${linklist}" | while IFS= read -r line; do
+  # 空行やコメント行はスキップ
+  if [ -z "${line}" ] || [ "${line:0:1}" = "#" ]; then
+    continue
+  fi
+
+  # target と link を分割
+  IFS=" " read -r target link <<< "${line}"
+
+  # 変数を展開
+  link="${link/#\~/${HOME}}"
+
+  # シンボリックリンクを作成
+  if ! __unlink "${link}"; then
+    echo "Error: Failed to create symbolic link: ${link}"
+  fi
 done
