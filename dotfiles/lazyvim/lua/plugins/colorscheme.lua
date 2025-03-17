6266033948937b67852c5ace4c78586f042723cd
ta-tabox <tanktabox@gmail.com>
2025-03-17 14:27:46 +0900

    lazyvim: 背景色の透過を辞める

--- dotfiles/lazyvim/lua/plugins/colorscheme.lua
+++ dotfiles/lazyvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
     "Mofiqul/dracula.nvim",
     opts = {
       -- 透明を有効化
-      transparent_bg = true, -- default false
+      -- transparent_bg = true, -- default false
     }
   },
   {
