c875e5465bb3a0ae6b6d20426eef4d7f86aa21ce
ta-tabox <tanktabox@gmail.com>
2026-04-07 13:19:45 +0900

    shell: avoid cwd changes

--- dotfiles/fish/conf.d/01_dotfiles_root.fish
+++ dotfiles/fish/conf.d/01_dotfiles_root.fish
@@ -1,16 +1,20 @@
 set _dotfiles_conf_path (status filename)
 if test -L "$_dotfiles_conf_path"
-    set _dotfiles_conf_path (readlink "$_dotfiles_conf_path")
+    set _dotfiles_link_target (readlink "$_dotfiles_conf_path")
+
+    if string match -qr '^/' -- "$_dotfiles_link_target"
+        set _dotfiles_conf_path "$_dotfiles_link_target"
+    else
+        set _dotfiles_conf_path (path resolve "$(path dirname "$_dotfiles_conf_path")/$_dotfiles_link_target")
+    end
 end
 
-set _dotfiles_root (begin
-    cd (dirname "$_dotfiles_conf_path")/.. 2>/dev/null
-    and pwd
-end)
+set _dotfiles_root (path resolve "$(path dirname "$_dotfiles_conf_path")/../..")
 
-if test -n "$_dotfiles_root"
+if test -d "$_dotfiles_root"
     set -gx DOTFILES_ROOT "$_dotfiles_root"
 end
 
 set -e _dotfiles_conf_path
+set -e _dotfiles_link_target
 set -e _dotfiles_root
--- dotfiles/zsh.d/01_dotfiles_root.zsh
+++ dotfiles/zsh.d/01_dotfiles_root.zsh
@@ -1,7 +1,16 @@
 _dotfiles_conf_path="${(%):-%x}"
 if [ -L "$_dotfiles_conf_path" ]; then
-  _dotfiles_conf_path="$(readlink "$_dotfiles_conf_path")"
+  _dotfiles_link_target="$(readlink "$_dotfiles_conf_path")"
+  if [[ "$_dotfiles_link_target" = /* ]]; then
+    _dotfiles_conf_path="$_dotfiles_link_target"
+  else
+    _dotfiles_conf_path="${_dotfiles_conf_path:A:h}/$_dotfiles_link_target"
+  fi
 fi
-export DOTFILES_ROOT="$(cd "$(dirname "$_dotfiles_conf_path")/.." 2>/dev/null && pwd)"
 
-unset _dotfiles_conf_path
+_dotfiles_root="${_dotfiles_conf_path:A:h:h}"
+if [ -d "$_dotfiles_root" ]; then
+  export DOTFILES_ROOT="$_dotfiles_root"
+fi
+
+unset _dotfiles_conf_path _dotfiles_link_target _dotfiles_root
