44f032c87ea13437dd0587b1ff3c0525490f2c75
ta-tabox <tanktabox@gmail.com>
2026-07-02 21:38:54 +0900

    claude: add path for claude

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -49,3 +49,6 @@ set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not contains $PNPM_HOME $PATH
     set -gx PATH $PNPM_HOME $PATH
 end
+
+# claude
+set -gx PATH $HOME/.local/bin $PATH
--- dotfiles/zsh.d/library.zsh
+++ dotfiles/zsh.d/library.zsh
@@ -15,3 +15,6 @@ export FZF_DEFAULT_COMMAND='fd --type f'
 
 # lazygitの設定パスを変更
 export XDG_CONFIG_HOME="$HOME/.config"
+
+# claude
+export PATH="$HOME/.local/bin:$PATH"
